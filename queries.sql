9c115942c3e4c83cf67dd0b1aaf0457062e82054
Ilias <iliaspaschop@gmail.com>
2025-05-02 21:17:58 +0300

    b3

--- queries.sql
+++ queries.sql
@@ -9,13 +9,38 @@ GROUP BY f.year, t.payment_method
 ORDER BY f.year, t.payment_method;
 
 -- b2: Βρείτε όλους τους καλλιτέχνες που ανήκουν σε ένα συγκεκριμένο μουσικό είδος με ένδειξη αν συμμετείχαν σε εκδηλώσεις του φεστιβάλ για το συγκεκριμένο έτος ;
-SELECT 
+SELECT
     ag.genre,
     a.artist_id,
     a.name
-FROM artist_genre ag JOIN artist a 
+FROM artist_genre ag JOIN artist a
 
-ON ag.artist_id = a.artist_id
+    ON ag.artist_id = a.artist_id
 
 ORDER BY 
     ag.genre, a.name;
+
+--b3
+SELECT
+    Artist.name AS artist_name,
+    Festival.year AS festival_year,
+    COUNT(Performance.performance_id) AS warm_up_count
+FROM Performance
+
+    JOIN Event ON Performance.event_id = Event.event_id
+
+    JOIN Festival ON Event.festival_id = Festival.festival_id
+
+    JOIN Artist ON Performance.artist_id = Artist.artist_id
+
+WHERE 
+    Performance.performance_type = 'warm up'
+
+GROUP BY 
+    Artist.name, Festival.year
+
+HAVING 
+    COUNT(Performance.performance_id) > 2;
+
+
+
