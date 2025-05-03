4bf717d4fb47d088241cf24b2bab85f5aa35d20c
perajacks <viper@june-23.com>
2025-05-03 19:11:55 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -85,8 +85,8 @@ GROUP BY
 
 
 -- b7: Βρείτε ποιο φεστιβάλ είχε τον χαμηλότερο μέσο όρο εμπειρίας τεχνικού προσωπικού;
-SELECT
-	f.festival_id,
+SELECT TOP 1
+    f.festival_id,
     ROUND(AVG(
         CASE s.experience_level
             WHEN 'ειδικευόμενος' THEN 1
@@ -96,15 +96,12 @@ SELECT
             WHEN 'πολύ έμπειρος' THEN 5
         END
     ), 2) AS avg_experience_score  
-	
 FROM Festival f
-JOIN Event e on f.festival_id = e.festival_id
-JOIN Event_Staff es on e.event_id = es.event_id
+JOIN Event e ON f.festival_id = e.festival_id
+JOIN Event_Staff es ON e.event_id = es.event_id
 JOIN Staff s ON es.staff_id = s.staff_id
-	
 GROUP BY f.festival_id
-ORDER BY avg_experience_score ASC
-LIMIT 1;
+ORDER BY avg_experience_score ASC;
 
 
 
