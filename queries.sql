9032b2b2beed8c332b00d4bf225ceb4a23755962
perajacks <viper@june-23.com>
2025-05-05 18:35:58 +0300

    b9 Update queries.sql

--- queries.sql
+++ queries.sql
@@ -208,6 +208,29 @@ FROM ArtistParticipation ap
 JOIN MaxParticipation mp ON 1 = 1
 WHERE ap.total_participations <= mp.max_participations - 5;
 
+-- b9:  Βρείτε ποιοι επισκέπτες έχουν παρακολουθήσει τον ίδιο αριθμό παραστάσεων σε διάστημα ενός έτους με περισσότερες από 3 παρακολουθήσεις;
+WITH yearly_counts AS (
+   SELECT
+       t.visitor_id,
+       YEAR(e.event_date) AS year,
+       COUNT(DISTINCT t.event_id) AS num_events
+   FROM Ticket t
+   JOIN Event e ON t.event_id = e.event_id
+   GROUP BY t.visitor_id, YEAR(e.event_date)
+),
+filtered_counts AS (
+   SELECT *
+   FROM yearly_counts
+   WHERE num_events >= 3
+)
+SELECT DISTINCT a.visitor_id, a.year, a.num_events
+FROM filtered_counts a
+JOIN filtered_counts b
+  ON a.year = b.year
+ AND a.num_events = b.num_events
+ AND a.visitor_id <> b.visitor_id
+ORDER BY a.year, a.num_events, a.visitor_id;
+
 
 
 
