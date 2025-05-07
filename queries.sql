77e55083b2c2986b9107fc71af79fce0515e26a9
perajacks <viper@june-23.com>
2025-05-07 03:18:05 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -261,6 +261,26 @@ FROM Security_Personnel sp
 JOIN Secondary_Personnel sec ON sp.event_id = sec.event_id
 ORDER BY sp.event_id;
 
+-- b13: Βρείτε τους καλλιτέχνες που έχουν συμμετάσχει σε φεστιβάλ σε τουλάχιστον 3 διαφορετικές ηπείρους.
+WITH Different_Continents AS (
+   SELECT
+       a.artist_id,
+       a.name,
+       COUNT(DISTINCT l.continent) AS distinct_continents
+   FROM festival f
+   JOIN location l ON f.location_id = l.location_id
+   JOIN event e ON f.festival_id = e.festival_id
+   JOIN performance p ON p.event_id = e.event_id
+   JOIN artist a ON p.artist_id = a.artist_id
+   GROUP BY a.artist_id
+),
+filtered_counts AS (
+   SELECT *
+   FROM Different_Continents
+   WHERE distinct_continents >= 3
+)
+SELECT * FROM filtered_counts;
+
 
 
 
