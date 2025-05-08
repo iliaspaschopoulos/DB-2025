40daaa80cfabfce69ee1fa4bdd8620a111a89dc3
perajacks <viper@june-23.com>
2025-05-08 14:23:16 +0300

    Update Q06.sql

--- Q06.sql
+++ Q06.sql
@@ -8,3 +8,26 @@ JOIN Performance p ON t.performance_id = p.performance_id
 LEFT JOIN Rating r ON r.performance_id = p.performance_id AND r.visitor_id = t.visitor_id
 WHERE t.visitor_id = 1
 GROUP BY p.performance_id;
+OPTION (LOOP JOIN);
+SELECT
+    p.performance_id,
+    AVG(
+        (r.interpretation_score + r.sound_lighting_score + r.stage_presence_score + r.organization_score) / 4.0
+    ) AS avg_rating_by_visitor
+FROM Ticket t
+JOIN Performance p ON t.performance_id = p.performance_id
+LEFT JOIN Rating r ON r.performance_id = p.performance_id AND r.visitor_id = t.visitor_id
+WHERE t.visitor_id = 1
+GROUP BY p.performance_id
+OPTION (HASH JOIN);
+SELECT
+    p.performance_id,
+    AVG(
+        (r.interpretation_score + r.sound_lighting_score + r.stage_presence_score + r.organization_score) / 4.0
+    ) AS avg_rating_by_visitor
+FROM Ticket t
+JOIN Performance p ON t.performance_id = p.performance_id
+LEFT JOIN Rating r ON r.performance_id = p.performance_id AND r.visitor_id = t.visitor_id
+WHERE t.visitor_id = 1
+GROUP BY p.performance_id
+OPTION (MERGE JOIN);
