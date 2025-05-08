1adb482c2c7249eb6db8e8f6b28780ed733c8476
perajacks <viper@june-23.com>
2025-05-08 14:21:54 +0300

    Create Q06.sql

---
+++ Q06.sql
@@ -0,0 +1,10 @@
+SELECT
+    p.performance_id,
+    AVG(
+        (r.interpretation_score + r.sound_lighting_score + r.stage_presence_score + r.organization_score) / 4.0
+    ) AS avg_rating_by_visitor
+FROM Ticket t
+JOIN Performance p ON t.performance_id = p.performance_id
+LEFT JOIN Rating r ON r.performance_id = p.performance_id AND r.visitor_id = t.visitor_id
+WHERE t.visitor_id = 1
+GROUP BY p.performance_id;
